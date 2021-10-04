c7075a69ac0c3b869b9ab43730e560251c7c6f21
Ball-Capper <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 09:57:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+#only for windows
 import webbrowser
+#uses the dev crash test thingy
 url = 'chrome://inducebrowsercrashforrealz'
+#runs chrome
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+#opens the dev url
 webbrowser.get(chrome_path).open(url) #crashes coom
