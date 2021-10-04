e5c9da7e1f4c6d4c13e328acf88db151e3db77d8
Ball-Capper <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 11:48:28 -0700

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,17 +1,13 @@
 import webbrowser
 import time
-#crashes chrome
 url = 'https://p.ksh3.tk/'
-#opens chrome
 print('Opening Chrome')
 time.sleep(3)
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 time.sleep(3)
 print('Done')
-#opens the crasher url
 print('Opening Crasher thing')
 time.sleep(3)
-#open it pp moment
 webbrowser.get(chrome_path).open(url) 
 time.sleep(3)
 print('Done')
