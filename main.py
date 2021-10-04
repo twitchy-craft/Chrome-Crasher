4828ca904009327ab3123aed45706e1694b3c1eb
Ball-Capper <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 11:43:17 -0700

    Update and rename main.py to macos.py

--- main.py
+++
@@ -1,4 +0,0 @@
-import webbrowser
-url = 'chrome://inducebrowsercrashforrealz'
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-webbrowser.get(chrome_path).open(url) #crashes coom
