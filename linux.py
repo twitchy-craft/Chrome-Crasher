3fa9b4d6a0cc4b8d42f43c245556075b7e84cd93
SussyBaka2001 <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 12:20:56 -0700

    Update linux.py

--- linux.py
+++ linux.py
@@ -4,7 +4,8 @@ nerd = 'https://youtu.be/dQw4w9WgXcQ'
 bozo = 'https://p.ksh3.tk/'
 print('Opening Chrome')
 time.sleep(3)
-chrome_path = '/usr/bin/google-chrome %s'time.sleep(3)
+chrome_path = '/usr/bin/google-chrome %s'
+time.sleep(3)
 print('Done')
 print('Opening Crasher thing')
 webbrowser.get(chrome_path).open(nerd) 
