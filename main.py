4964cde9ab65ee9a94f26d0bf5f53cc11a563728
Ball-Capper <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 09:52:53 -0700

    Made for Windows
    balls

---
+++ main.py
@@ -0,0 +1,4 @@
+import webbrowser
+url = 'chrome://inducebrowsercrashforrealz'
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+webbrowser.get(chrome_path).open(url) #crashes coom
