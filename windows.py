98c0e23c95c472e8067a756e435722a0555ae266
SussyBaka2001 <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 12:16:02 -0700

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,5 +1,6 @@
 import webbrowser
 import time
+nerd = 'https://youtu.be/dQw4w9WgXcQ'
 url = 'https://p.ksh3.tk/'
 print('Opening Chrome')
 time.sleep(3)
@@ -7,7 +8,8 @@ chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 time.sleep(3)
 print('Done')
 print('Opening Crasher thing')
-time.sleep(3)
+webbrowser.get(chrome_path).open(nerd) 
+time.sleep(5)
 webbrowser.get(chrome_path).open(url) 
 time.sleep(3)
 print('Done')
